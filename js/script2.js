ec25b3102c57ba00c272a1fcf009527405ac8f8f
kapitoshka1 <43035500+kapitoshka1@users.noreply.github.com>
2019-04-09 19:10:03 +0300

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 var elems = document.querySelectorAll('.slider');
 var instances = M.Slider.init(elems, {'height' : 300, 'indicators' : false});
 });
+
+
+
+
